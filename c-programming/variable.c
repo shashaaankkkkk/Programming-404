072c258546297c6f785995e903d27fd01cea2399
shashaaankkkkk <shashankshekhar8534@gmail.com>
2023-12-23 19:51:56 +0530

    Day 2 Variables

---
+++ c-programming/variable.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+  int a;
+  float b;
+  char c;
+  double d;
+  printf("size of Int is %lu\n",sizeof(a));
+  printf("Size of Float is %lu\n",sizeof(b));
+  printf("Size of Char is %lu\n",sizeof(c));
+  printf("Size of Double is %lu\n",sizeof(d));
+}
+
