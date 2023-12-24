213ae7c571571eadd7b2a0a23d34c3be23bc6a08
shashaaankkkkk <shashankshekhar8534@gmail.com>
2023-12-24 18:38:52 +0530

    Day 3 Operators

---
+++ c-programming/operator.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+/*Operators are used to perform operations on variables and values*/
+  /*
+Arithmetic operators(+,-,/,*,++,--,%)
+Assignment operators(=,+=,-=,*=) 
+Comparison operators(==,!=,<,>,<=,>=)
+Logical operators(&& , || , ! )
+Bitwise operators
+  */
+  int a=1;
+  int b=1;
+  printf("sum is %d\n",a+b);
+  printf("difference is  %d\n",a-b);
+  printf("product is %d\n" ,a*b);
+  printf("division is %d\n",a/b);
+  printf("Remainder is %d",a%b);
+
+}
