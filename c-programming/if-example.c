4657de1c654fe6546c2dc73ef8776ff172b108cc
shashaaankkkkk <shashankshekhar8534@gmail.com>
2023-12-26 20:16:38 +0530

    Day 6 implementation of if else

---
+++ c-programming/if-example.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+  //program to check age of user 
+  int age;
+  printf("enter your age\n");
+  scanf("%d",&age);
+  if (age<18){
+    printf("you are nabalik\n");
+
+  }else{
+    printf("jao sarkar girrao");
+  }
+
+}
